3080a2f59afafa7e8b81ab596906e080d657420b
chengzhxu <chengzhxu@163.com>
2020-09-06 00:39:58 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "Bird_Bee_Api/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
